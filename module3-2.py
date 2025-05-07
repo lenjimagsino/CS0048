ccb4ae40ba690a61cebfb099b333af99c3699620
Lenj Jesther Magsino <mlenjjesther18@gmail.com>
2025-05-07 12:36:43 +0800

    module3-2.py

--- module3-2.py
+++ module3-2.py
@@ -1,29 +1,45 @@
-#Temperature Converter
-
-def temperature_converter():
+def menu():
     while True:
         print("\nTemperature Converter Menu:")
-        print("1. Convert Celsius to Fahrenheit")
-        print("2. Convert Fahrenheit to Celsius")
+        print("1. Celsius to Fahrenheit")
+        print("2. Fahrenheit to Celsius")
         print("3. Exit")
 
         choice = input("Enter your choice (1-3): ")
 
-        if choice == '3':
-            print("Exiting temperature converter. Goodbye!")
+        if choice == '1':
+            convert_c_to_f()
+        elif choice == '2':
+            convert_f_to_c()
+        elif choice == '3':
+            print("Exiting temperature converter.")
             break
-
-        temp = input("Enter temperature: ")
-
-        if temp.replace('.', '', 1).isdigit() or (temp.startswith('-') and temp[1:].replace('.', '', 1).isdigit()):
-            t = float(temp)
-            if choice == '1':
-                print("Temperature in Fahrenheit:", (t * 9 / 5) + 32)
-            elif choice == '2':
-                print("Temperature in Celsius:", (t - 32) * 5 / 9)
-            else:
-                print("Invalid choice.")
         else:
-            print("Please enter a valid number.")
-            
-temperature_converter()
+            print("Invalid choice.")
+
+def convert_c_to_f():
+    temp = input("Enter temperature in Celsius: ")
+    if is_number(temp):
+        c = float(temp)
+        f = (c * 9 / 5) + 32
+        print("Fahrenheit:", f)
+    else:
+        print("Invalid number.")
+
+def convert_f_to_c():
+    temp = input("Enter temperature in Fahrenheit: ")
+    if is_number(temp):
+        f = float(temp)
+        c = (f - 32) * 5 / 9
+        print("Celsius:", c)
+    else:
+        print("Invalid number.")
+
+def is_number(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+menu()
