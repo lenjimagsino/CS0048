fe0e92decec8c4bcd6d8cff2fa01e4aa17730f49
Lenj Jesther Magsino <mlenjjesther18@gmail.com>
2025-05-07 12:37:19 +0800

    module3-4.py

--- module3-4.py
+++ module3-4.py
@@ -1,37 +1,40 @@
-#Number Guessing Game
-
 import random
 
-def number_guessing_game():
+def menu():
     while True:
         print("\nNumber Guessing Game Menu:")
-        print("1. Play Number Guessing Game")
+        print("1. Play")
         print("2. Exit")
 
         choice = input("Enter your choice (1-2): ")
 
-        if choice == '2':
-            print("Exiting game. Goodbye!")
+        if choice == '1':
+            play_game()
+        elif choice == '2':
+            print("Exiting game.")
             break
-        elif choice == '1':
-            number = random.randint(1, 100)
-            attempts = 0
-            print("Guess a number between 1 and 100.")
-            while True:
-                guess = input("Guess the number: ")
-                if guess.isdigit():
-                    guess = int(guess)
-                    attempts += 1
-                    if guess < number:
-                        print("Too low!")
-                    elif guess > number:
-                        print("Too high!")
-                    else:
-                        print(f"Congratulations! You guessed the number in {attempts} attempts.")
-                        break
-                else:
-                    print("Please enter a valid number.")
         else:
-            print("Invalid choice. Please enter 1 or 2.")
+            print("Invalid choice.")
 
-number_guessing_game()
+def play_game():
+    number = random.randint(1, 100)
+    attempts = 0
+
+    print("Guess a number between 1 and 100.")
+
+    while True:
+        guess = input("Your guess: ")
+        if guess.isdigit():
+            guess = int(guess)
+            attempts += 1
+            if guess < number:
+                print("Too low!")
+            elif guess > number:
+                print("Too high!")
+            else:
+                print(f"Congratulations! You guessed the number in {attempts} attempts.")
+                break
+        else:
+            print("Please enter a valid number.")
+
+menu()
